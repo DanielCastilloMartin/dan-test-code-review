244291d2f6f937769556f091ee21b27977846da4
Daniel Del Castillo Martín <daniel.delcastillo@optimissa.com>
2022-03-12 11:44:40 +0100

    .gitignore is not working

---
+++ src/main/java/es/danie/test/etalentum/danjavatest/DanJavaTestApplication.java
@@ -0,0 +1,13 @@
+package es.danie.test.etalentum.danjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanJavaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/danie/test/etalentum/danjavatest/DanJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.danie.test.etalentum.danjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanJavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
