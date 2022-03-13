13cbf85020228477383634cf650c23418503fc2e
Daniel Del Castillo Martín <daniel.delcastillo@optimissa.com>
2022-03-13 18:38:42 +0100

    We filter the list by all fields like "like"

--- src/main/java/es/daniel/test/etalentum/danijavatest/controller/impl/CollectionsController.java
+++ src/main/java/es/daniel/test/etalentum/danijavatest/controller/impl/CollectionsController.java
@@ -4,7 +4,6 @@ import es.daniel.test.etalentum.danijavatest.controller.ICollectionsController;
 import es.daniel.test.etalentum.danijavatest.service.ICollectionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
